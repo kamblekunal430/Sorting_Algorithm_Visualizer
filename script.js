d1d432cebeabdaf0d55cb35b54e71c84766c8b10
kamblekunal430 <kamblekunal430@gmail.com>
2021-03-20 22:17:50 +0530

    implemented the bar visual for ramdom array

---
+++ script.js
@@ -0,0 +1,21 @@
+var flag = 0;
+
+function createArray() {
+  var randomArray = [];
+  for (let i = 0; i < 50; i++) {
+    randomArray.push(Math.floor(Math.random() * 250 + 10));
+  }
+  console.log(randomArray);
+  document.getElementById("bars").innerHTML = "";
+  createBars(randomArray);
+}
+function createBars(randomArray) {
+  for (let i = 0; i < 50; i++) {
+    var arr_bar = document.createElement("div");
+    arr_bar.id = "div" + i.toString();
+    arr_bar.className = "barStyle";
+    arr_bar.style.height = `${randomArray[i]}px`;
+    arr_bar.innerHTML = randomArray[i];
+    document.getElementById("bars").appendChild(arr_bar);
+  }
+}
